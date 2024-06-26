cffdcbdb7682f44ddefffe2016c6a6050083d318
Italo <itallo016@gmail.com>
2024-06-26 20:56:37 -0300

    Projeto Key

---
+++ Programa Base com Key/KeysBase/F_KeyLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeysBase
+{
+    public partial class F_KeyLogin : Form
+    {
+        public F_KeyLogin()
+        {
+            InitializeComponent();
+        }
+
+        private int VerificaKey()
+        {
+            var recebeValor = 0;
+            if(recebeValor == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ Programa Base com Key/KeysBase/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeysBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ChamarKey();
+        }
+
+        private void ChamarKey()
+        {
+            this.Hide();
+            F_KeyLogin f_KeyLogin = new F_KeyLogin();
+            f_KeyLogin.ShowDialog();
+        }
+    }
+}
